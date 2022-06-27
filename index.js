78e22d79fd04966c00821ee5e1b5a3133e330854
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-06-27 14:52:48 +0600

    connect to database with secure username and password

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}1aTa6ZOOnkSL90xQ@cluster0.myfuo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('warehouse management DB connected ');
+  // perform actions on the collection object
+  client.close();
+});
+
+
 // middleware
 app.use(cors());
 app.use(express.json());
