b67ca0cf6a5fcf957764d260dad2502d96096551
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-06-27 15:53:55 +0600

    create two api and load them from client side

--- index.js
+++ index.js
@@ -2,24 +2,44 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}1aTa6ZOOnkSL90xQ@cluster0.myfuo.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('warehouse management DB connected ');
-  // perform actions on the collection object
-  client.close();
-});
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.myfuo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+      await client.connect()
+      const productCollection = client.db('warehouseManagement').collection('items');
+
+      app.get('/item', async(req, res) => {
+        const query = {};
+        const cursor = productCollection.find(query);
+        const items = await cursor.toArray();
+        res.send(items)
+      });
+
+      app.get('/item/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const item = await productCollection.findOne(query);
+        res.send(item)
+      })
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir)
+
+
 
 app.get('/', (req, res) =>{
     res.send('running warehouse management server ')
