6ca26ea89a9244e3e017ddb62c5131bc8d0418ff
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-06-27 14:15:46 +0600

    setup a simple server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('running warehouse management server ')
+});
+
+app.listen(port, ()=>{
+    console.log('server running');
+})
